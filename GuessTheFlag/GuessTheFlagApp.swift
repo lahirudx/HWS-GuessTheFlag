478e9ef8cb8f2d360ac49dec9a229a9a8837b376
Lahiru Amarasooriya <lahirudx@gmail.com>
2023-08-16 07:33:21 +0530

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Lahiru Amarasooriya on 2023-08-16.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
