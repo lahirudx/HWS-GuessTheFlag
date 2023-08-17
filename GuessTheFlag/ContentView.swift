486378facf2afea6f1461c3f9281a60bdaf9f6e9
Lahiru Amarasooriya <lahirudx@gmail.com>
2023-08-17 08:10:20 +0530

    Completed the project with challenges

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,14 +8,111 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var scoreAlertMessage = ""
+    @State private var score = 0
+    @State private var retryCount = 0
+    @State private var showRetryAlert = false
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 400)
+            .ignoresSafeArea()
+            
+            VStack {
+                Text("Guess the Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundColor(.white)
+                
+                Spacer()
+                
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .renderingMode(.original)
+                                .cornerRadius(10)
+                                .shadow(radius: 5)
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: \(score)")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                
+                Spacer()
+            
         }
-        .padding()
+            .padding()
+            
+        }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text(scoreAlertMessage)
+        }
+        .alert("Restart Game", isPresented: $showRetryAlert) {
+            Button("Reset", action: resetTheGame)
+        } message: {
+            Text("You have scored \(score).")
+        }
+    }
+    
+    func flagTapped(_ number: Int) {
+        
+        retryCount += 1
+        
+        if retryCount >= 8 {
+            showRetryAlert.toggle()
+        } else {
+            if number == correctAnswer {
+                scoreTitle = "Correct"
+                scoreAlertMessage = "Your score is \(score)"
+                score += 1
+            } else {
+                scoreTitle = "Wrong"
+                scoreAlertMessage = "Wrong! That’s the flag of \(countries[number])"
+            }
+
+            showingScore = true
+        }
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func resetTheGame() {
+        askQuestion()
+        retryCount = 0
+        score = 0
     }
 }
 
